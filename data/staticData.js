e418add8a29b12f116e777c7ff0b8715b96853cd
Gega-Jikuridze <gega.jikuridze.1998@gmail.com>
2025-03-26 06:08:54 -0700

    Header

---
+++ data/staticData.js
@@ -0,0 +1,85 @@
+// header 
+
+export const headerNav = [
+    {
+        name:'Home',
+        classes:['bx', 'bxs-home'],
+        link:'#',
+    },
+    {
+        name:'Movies',
+        classes:['bx', 'bxs-movie-play'],
+        link:'#',
+        subLinks:[
+            {
+                name:'Action',
+                link:'#'
+            },
+            {
+                name:'Scary',
+                link:'#'
+            },
+            {
+                name:'Trailer',
+                link:'#'
+            },
+        ]
+    },
+    {
+        name:'TV Shows',
+        classes:['bx', 'bxs-film'],
+        link:'#',
+        subLinks:[
+            {
+                name:'Action',
+                link:'#'
+            },
+            {
+                name:'Scary',
+                link:'#'
+            },
+            {
+                name:'Trailer',
+                link:'#'
+            },
+        ]
+    },
+    {
+        name:'People',
+        classes:['bx', 'bx-male-female'],
+        link:'#',
+        subLinks:[
+            {
+                name:'Action',
+                link:'#'
+            },
+            {
+                name:'Scary',
+                link:'#'
+            },
+            {
+                name:'Trailer',
+                link:'#'
+            },
+        ]
+    },
+    {
+        name:'More',
+        classes:['bx', 'bxs-hand-down'],
+        link:'#',
+        subLinks:[
+            {
+                name:'Action',
+                link:'#'
+            },
+            {
+                name:'Scary',
+                link:'#'
+            },
+            {
+                name:'Trailer',
+                link:'#'
+            },
+        ]
+    },
+]
